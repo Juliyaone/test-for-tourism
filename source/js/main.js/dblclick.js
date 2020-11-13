fe125dc750fd894edf968e3200939da4af780cfe
ivanpc\Юлия <Juliyaone123@gmail.com>
2020-11-13 19:14:21 +0300

    Валидация костыль

--- source/js/main.js/dblclick.js
+++ source/js/main.js/dblclick.js
@@ -13,64 +13,50 @@ $(document).ready(function() {
     var valueForAttr = $(this).text();
 
     //Добавляем форму для редактирования телефона
-    $(this).append( "<form action='#' id='myForm' enctype='multipart/form-data'><input type='tel' id='phone' class='telephone-example' name='phone' minlength ='18' required /><label class='phone-label visually-hidden' for='phone'>Заполните это поле</label><input class='form-submit' type='submit' value='Сохранить' /></form>" );
+    $(this).append( "<form action='#' id='myForm' enctype='multipart/form-data'><input type='tel' id='phone' class='telephone-example' name='phone' minlength ='18' required /><label class='phone-error visually-hidden' for='phone'>Заполните это поле</label><input class='form-submit' type='submit' value='Сохранить'/></form>" );
 
     // Добавляем атрибут value в поле ввода телефона и добавляем атрибуту данные из ячейки
 
-    $(this).find( '#phone' ).attr( "value", valueForAttr);
-    $(this).find( '#phone' ).attr( "placeholder", "+7-(999)-999-99-99");
+    $(this).find( "#phone" ).attr( "value", valueForAttr);
+    $(this).find( "#phone" ).attr( "placeholder", "+7-(999)-999-99-99");
 
 
     // Добавляем маску на поле с номером телефона
-    $('#phone').inputmask('+7(999)-999-99-99');
+    $( "#phone" ).inputmask('+7(999)-999-99-99');
 
     // Ставим курсор сразу в поле для редактирования телефона
     $( "#phone" ).focus();
 
-    // На поле с телефоном навешиваем обработчик события по потере фокуса
-    $('#phone').blur( function(evt) {
-      // Находим введенные в поле данные
-      var dataInput = $('#phone').serializeArray();
 
-      // Находим value 1 элемента массива dataInput
-      var dataInputValue = dataInput[0].value;
 
-      if (dataInputValue.search('_') > -1 || dataInputValue ==='+7-(___)-___-___-__' || dataInputValue === '') {
-        $(".telephone-example").addClass("invalid");
-        $(".phone-label").removeClass("visually-hidden");
 
-      } else {
-        console.log(dataInputValue);
-        $('#myForm').remove();
-      }
-    });
+
+
+
 
 
 
     // При нажатии на кнопку submit форма отправит данные и удалится
-    $('#myForm').submit( function(evt) {
+    $( "#myForm" ).submit( function(evt) {
       evt.preventDefault();
+      var dataInput = $( "#phone" ).val();
+      console.log(dataInput);
+      validate(dataInput);
+      // console.log(dataInputValue);
+      // $(this).submit();
       // $('#myForm').remove();
-      return false;
+      // return false;
     });
 
+
+
     // При нажатии на enter форма отправит данные и удалится
-    $('#table_id').on('keydown', function(evt) {
-
-      // Находим введенные в поле данные
-      var dataInput = $('#phone').serializeArray();
-
-      // Находим value 1 элемента массива dataInput
-      var dataInputValue = dataInput[0].value;
-
+    $( "#phone" ).on('keydown', function(evt) {
+      // evt.preventDefault();
       if (evt.keyCode === 13) {
-
-        if (dataInputValue.search('_') > -1 || dataInputValue ==='+7-(___)-___-___-__' || dataInputValue === '') {
-          $(".telephone-example").addClass("invalid");
-          $(".phone-label").removeClass("visually-hidden");
-        }
-
-
+        var dataInput = $( "#phone" ).val();
+        console.log(dataInput);
+        validate(dataInput);
         // console.log(dataInputValue);
         // $(this).submit();
         // $('#myForm').remove();
